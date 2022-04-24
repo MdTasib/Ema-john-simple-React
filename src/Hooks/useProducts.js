579749ca0d61e4e8c080d3b2e3400df19ad58558
MdTasib <ohidulalam442@gmail.com>
2022-04-24 16:23:53 +0600

    load data on the server

--- src/Hooks/useProducts.js
+++ src/Hooks/useProducts.js
@@ -5,7 +5,7 @@ const useProducts = () => {
 
 	useEffect(() => {
 		(async () => {
-			const res = await fetch("products.json");
+			const res = await fetch("http://localhost:5000/product");
 			const data = await res.json();
 			setProducts(data);
 		})();
